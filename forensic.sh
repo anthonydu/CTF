e0aa483fc57320d2dc368a2b46ca9c5b3835948f
Anthony Du <github@anthonydu.com>
2023-08-12 20:09:02 +0800

    Update forensic.sh

--- forensic.sh
+++ forensic.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 function forensic {
     if [ "$#" -eq 1 ]
     then
@@ -8,20 +10,29 @@ function forensic {
         read -p "Would you like to carve this file? (y/n) " carve &&
         case "$carve" in
             [yY] )
-                scalpel -c ~/scalpel.conf -o scalpel_"$1"_$(date +%s) "$1";
-                foremost -o foremost_"$1"_$(date +%s) "$1";;
+                local DIR_NAME="$1"_$(date +%s)_carved &&
+                mkdir "$DIR_NAME" &&
+                scalpel -c ~/scalpel.conf -o "$DIR_NAME"/scalpel "$1" &&
+                foremost -o "$DIR_NAME"/foremost "$1";;
             * )
-                echo "File carving aborted.";;
+                echo "File not carved.";;
         esac &&
-        read -p "Would you like to string this file? (y/n) " strings &&
+        read -p "Would you like to extract strings from this file? (y/n) " strings &&
         case "$strings" in
             [yY] )
-                strings "$1";;
+                strings -n 5 "$1";;
             * )
-                echo "Strings command aborted.";;
+                echo "Strings not extracted.";;
+        esac &&
+        read -p "Would you like to check out this file's header? (y/n) " head &&
+        case "$head" in 
+            [yY] )
+                xxd "$1" | head;;
+            * )
+                echo "Header not displayed.";;
         esac
     else
-        echo "Usage: forensic [filename]"
+        echo -e "Usage: forensic \e[3mFILE\e[0m"
     fi
 }
 
