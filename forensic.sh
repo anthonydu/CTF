13c089a970b4a381b7926d8dc9cb4fe6145404d8
Anthony Du <github@anthonydu.com>
2023-08-12 17:56:33 +0800

    Update forensic.sh

--- forensic.sh
+++ forensic.sh
@@ -1,18 +1,19 @@
-function forensic() {
+function forensic {
     if [ "$#" -eq 1 ]
     then
         file "$1" &&
+        echo &&
         exiftool "$1" &&
         binwalk "$1" &&
-        read -p "Would you like to carve this file? (y/n) " carve
+        read -p "Would you like to carve this file? (y/n) " carve &&
         case "$carve" in
             [yY] )
                 scalpel -c ~/scalpel.conf -o scalpel_"$1"_$(date +%s) "$1";
                 foremost -o foremost_"$1"_$(date +%s) "$1";;
             * )
                 echo "File carving aborted.";;
-        esac
-        read -p "Would you like to string this file? (y/n) " strings
+        esac &&
+        read -p "Would you like to string this file? (y/n) " strings &&
         case "$strings" in
             [yY] )
                 strings "$1";;
@@ -20,7 +21,8 @@ function forensic() {
                 echo "Strings command aborted.";;
         esac
     else
-        echo "Usage: forensic [filename]";
+        echo "Usage: forensic [filename]"
     fi
 }
 
+forensic "$@";
